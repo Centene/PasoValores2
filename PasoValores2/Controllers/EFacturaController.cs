05027b80bb2d97c9d8b8c017f4fb62b78dc0e5b7
ivann3 <ivann3@RECEPCION>
2017-01-13 17:35:16 +0100

    Formulario

---
+++ PasoValores2/Controllers/EFacturaController.cs
@@ -0,0 +1,34 @@
+﻿using PasoValores2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PasoValores2.Controllers
+{
+    public class EFacturaController : Controller
+    {
+        static string FECHAFRA;
+        static string SERIE;
+        // GET: EFactura
+        public ActionResult Index(string serie, string fechafra )
+        {
+            EFactura fra = new EFactura();
+            FECHAFRA = fechafra;
+            SERIE = serie;
+            fra.Fecha = FECHAFRA;
+            fra.Serie = SERIE;
+            return View(fra);
+        }
+        //[HttpPost]
+        //public ActionResult Index(string fecha)
+        //{
+        //    EFactura fra = new EFactura();
+        //    fra.Fecha = FECHAFRA;
+        //    fra.Serie = SERIE;
+        //    //fra.Importe = Convert.ToInt32(importe);
+        //    return View();
+        //}
+    }
+}
--- PasoValores2/Controllers/HomeController.cs
+++ PasoValores2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PasoValores2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,25 @@ namespace PasoValores2.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Facturacion()
+        {
+            EFactura factura = new EFactura();
+            factura.Fecha = DateTime.Now.Date.ToString();
+            return View(factura);
+        }
+        [HttpPost]
+        public ActionResult Facturacion(string Serie, DateTime fecha)
+        {
+            EFacturaController otrafra = new EFacturaController();
+
+            string fechita;
+            EFactura factura = new EFactura();
+            factura.Serie = Serie;
+            fechita = fecha.ToShortDateString();
+            
+
+            return RedirectToAction("Index", "EFactura", new { serie= Serie, fechafra = fechita});
         }
     }
 }
